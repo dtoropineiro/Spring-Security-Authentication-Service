716aa75810e8ef0d7077ce878a227f4526fdf945
dtoropineiro <dtoropineiro@gmail.com>
2020-12-29 10:03:46 -0300

    first commit

---
+++ src/main/java/com/dtoropineiro/authjwtservice/AuthJwtServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dtoropineiro.authjwtservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthJwtServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthJwtServiceApplication.class, args);
+	}
+
+}
